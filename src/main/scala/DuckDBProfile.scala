f95666d3b5ff6385d4631b84ce3c441741d11c55
Algebrazebra <Algebrazebra@users.noreply.github.com>
2025-08-21 23:16:28 +0300

    Moved Upsert Builder and deleted MyPostgresProfile

--- src/main/scala/DuckDBProfile.scala
+++ src/main/scala/DuckDBProfile.scala
@@ -1,6 +1,6 @@
 package duckdbslick
 
-import builders.{DuckDBColumnDDLBuilderComponent, DuckDBQueryBuilderComponent, DuckDBTableDDLBuilderComponent}
+import builders.{DuckDBColumnDDLBuilderComponent, DuckDBQueryBuilderComponent, DuckDBTableDDLBuilderComponent, DuckDBUpsertBuilderComponent}
 import slick.SlickException
 import slick.ast.*
 import slick.basic.Capability
@@ -30,7 +30,8 @@ trait DuckDBProfile
     with MultipleRowsPerStatementSupport
     with DuckDBQueryBuilderComponent
     with DuckDBTableDDLBuilderComponent
-    with DuckDBColumnDDLBuilderComponent {
+    with DuckDBColumnDDLBuilderComponent
+    with DuckDBUpsertBuilderComponent {
 
   override def defaultTables(implicit
       ec: ExecutionContext
@@ -165,44 +166,6 @@ trait DuckDBProfile
   // We need to override base UpsertBuilder, because it's implemented using `MERGE` which DuckDB doesn't support.
   override def createUpsertBuilder(node: Insert): InsertBuilder =
     new DuckDBUpsertBuilder(node)
-
-  class DuckDBUpsertBuilder(insert: Insert) extends UpsertBuilder(insert) {
-    override def buildInsert: InsertBuilderResult = {
-      if (pkNames.isEmpty) {
-        throw new SlickException("Primary key required for insertOrUpdate")
-      }
-      val pkCols            = pkNames.mkString(", ")
-      val updateAssignments = softNames
-        .map(fs => s"$fs = EXCLUDED.$fs")
-        .mkString(", ")
-      val conflictAction    =
-        if (updateAssignments.isEmpty) "do nothing"
-        else "do update set " + updateAssignments
-      val insertSql         =
-        s"""insert into $tableName ${allNames.mkString("(", ", ", ")")}
-           |values $allVars
-           |on conflict ($pkCols)
-           |$conflictAction
-           |""".stripMargin.replaceAll("\n", " ")
-      new InsertBuilderResult(table, insertSql, allFields) {
-        override def buildMultiRowInsert(size: Int): String = {
-          // Generate placeholders per row (e.g., `(?, ?, ?)` with allVars)
-          val rowPlaceholder      = allVars
-          // Create placeholders for all rows
-          val multiRowPlaceholder =
-            List.fill(size)(rowPlaceholder).mkString(", ")
-
-          // Generate the multi-row insert SQL
-          s"""insert into $tableName ${allNames.mkString("(", ", ", ")")}
-             |values $multiRowPlaceholder
-             |on conflict ($pkCols)
-             |$conflictAction
-             |""".stripMargin.replaceAll("\n", " ")
-        }
-
-      }
-    }
-  }
 }
 
 object DuckDBProfile extends DuckDBProfile
---
+++ src/main/scala/builders/DuckDBUpsertBuilderComponent.scala
@@ -0,0 +1,51 @@
+package builders
+
+import slick.SlickException
+import slick.ast.*
+import slick.jdbc.{InsertBuilderResult, JdbcProfile}
+
+trait DuckDBUpsertBuilderComponent {
+  self: JdbcProfile =>
+
+  /** Builder for UPSERT statements.
+   *
+   * We need to override base UpsertBuilder, because it's implemented using `MERGE` which DuckDB doesn't support.
+   * This implementation uses DuckDB's `INSERT ... ON CONFLICT` syntax instead.
+   */
+  class DuckDBUpsertBuilder(insert: Insert) extends UpsertBuilder(insert) {
+    override def buildInsert: InsertBuilderResult = {
+      if (pkNames.isEmpty) {
+        throw new SlickException("Primary key required for insertOrUpdate")
+      }
+      val pkCols            = pkNames.mkString(", ")
+      val updateAssignments = softNames
+        .map(fs => s"$fs = EXCLUDED.$fs")
+        .mkString(", ")
+      val conflictAction    =
+        if (updateAssignments.isEmpty) "do nothing"
+        else "do update set " + updateAssignments
+      val insertSql         =
+        s"""insert into $tableName ${allNames.mkString("(", ", ", ")")}
+           |values $allVars
+           |on conflict ($pkCols)
+           |$conflictAction
+           |""".stripMargin.replaceAll("\n", " ")
+      new InsertBuilderResult(table, insertSql, allFields) {
+        override def buildMultiRowInsert(size: Int): String = {
+          // Generate placeholders per row (e.g., `(?, ?, ?)` with allVars)
+          val rowPlaceholder      = allVars
+          // Create placeholders for all rows
+          val multiRowPlaceholder =
+            List.fill(size)(rowPlaceholder).mkString(", ")
+
+          // Generate the multi-row insert SQL
+          s"""insert into $tableName ${allNames.mkString("(", ", ", ")")}
+             |values $multiRowPlaceholder
+             |on conflict ($pkCols)
+             |$conflictAction
+             |""".stripMargin.replaceAll("\n", " ")
+        }
+      }
+    }
+  }
+}
