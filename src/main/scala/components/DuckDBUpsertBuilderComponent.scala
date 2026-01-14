5c4e9dc0c15728ec20ed624a78b717c5d6724b84
Algebrazebra <Algebrazebra@users.noreply.github.com>
2026-01-14 20:59:11 +0100

    wip

--- src/main/scala/components/DuckDBUpsertBuilderComponent.scala
+++ src/main/scala/components/DuckDBUpsertBuilderComponent.scala
@@ -1,7 +1,8 @@
 package components
 
 import slick.SlickException
-import slick.ast.Insert
+import slick.ast.ColumnOption.{AutoInc, PrimaryKey}
+import slick.ast.{FieldSymbol, Insert, Node, Select}
 import slick.jdbc.{InsertBuilderResult, JdbcProfile}
 
 trait DuckDBUpsertBuilderComponent {
@@ -14,6 +15,48 @@ trait DuckDBUpsertBuilderComponent {
     * `INSERT ... ON CONFLICT` syntax instead.
     */
   class DuckDBUpsertBuilder(insert: Insert) extends UpsertBuilder(insert) {
+
+    private lazy val (nonPkAutoIncSyms, insertingSyms) = syms.toSeq.partition(sym => isAutoInc(sym) && !isPrimaryKey(sym))
+    override lazy val (pkSyms, softSyms) = insertingSyms.partition(sym => isPrimaryKey(sym) || isPartOfFuncDefinedPk(sym))
+
+    override def buildInsert: InsertBuilderResult = {
+      val insertSql = buildUpsertSql(tableName, insertingSyms, softSyms)
+      new InsertBuilderResult(table, insertSql, syms)
+    }
+
+    override def transformMapping(n: Node) = reorderColumns(n,
+      insertingSyms ++ nonPkAutoIncSyms ++ nonPkAutoIncSyms ++ nonPkAutoIncSyms)
+
+    private def buildUpsertSql(tableName: String, insertingSyms: Seq[FieldSymbol], softSyms: Seq[FieldSymbol]): String = {
+      val insertingColNames = insertingSyms.map(fs => quoteIdentifier(fs.name)).mkString("(", ", ", ")")
+      val insertingValuePlaceholders = insertingSyms.map(_ => "?").mkString("(", ", ", ")")
+      val primaryKeyNames = pkSyms.map(fs => quoteIdentifier(fs.name)).mkString("(", ",", ")")
+      val softNames = softSyms.map(fs => quoteIdentifier(fs.name))
+      val padding = if (nonPkAutoIncSyms.isEmpty) "" else "where ? is null or ?=?" // padding for JDBC parameters
+      val doUpdateOrNothing = if (softNames.isEmpty) "DO NOTHING" else
+        "DO UPDATE SET " + softNames.map(n => s"$n=EXCLUDED.$n").mkString(",")
+      s"""
+         |INSERT INTO $tableName $insertingColNames VALUES $insertingValuePlaceholders
+         |ON CONFLICT $primaryKeyNames $padding $doUpdateOrNothing
+         |""".stripMargin
+    }
+
+    private def isAutoInc(sym: FieldSymbol) = sym.options.contains(AutoInc)
+
+    private def isPrimaryKey(sym: FieldSymbol) = sym.options.contains(PrimaryKey)
+
+    private def isPartOfFuncDefinedPk(sym: FieldSymbol) = {
+      val funcDefinedPKs = table.profileTable.asInstanceOf[Table[?]].primaryKeys
+      funcDefinedPKs.exists(pk => pk.columns.collect {
+        case Select(_, f: FieldSymbol) => f
+      }.exists(_.name == sym.name))
+    }
+
+
+  }
+
+
+  class DuckDBUpsertBuilderV1(insert: Insert) extends UpsertBuilder(insert) {
     override def buildInsert: InsertBuilderResult = {
       if (pkNames.isEmpty) {
         throw new SlickException("Primary key required for insertOrUpdate")
